61b412b242a7cf985a7b564293ea2af3fc45d57d
bjk12 <45553346+bjk12@users.noreply.github.com>
2021-04-20 10:05:07 +0800

    Update DBSCANdemo.m

--- current_test/DBSCANdemo.m
+++ current_test/DBSCANdemo.m
@@ -1,21 +1,23 @@
-% DBSCAN �ܶȾ����㷨,XΪҪ�����������[һ��һ����,һ��һ����],epslionΪ��Χ��ֵ��MinPtsΪ��ֵ��Χ���ٽ������
+% DBSCAN 密度聚类算法,X为要聚类的数据组[一行一样本,一列一特征],epslion为范围阈值，MinPts为阈值范围内临近点个数
 function [IDX, isnoise]=DBSCANdemo(X,epsilon,MinPts)  
     C=0;
     n=size(X,1);
-    %�����ĵ�ͱ߽�� 
+    %含核心点和边界点 
     IDX=zeros(n,1);
     D=pdist2(X,X);
     visited=false(n,1);
-    %������������߽��
+    %噪声点和少数边界点
     isnoise=false(n,1);
     for i=1:n
         if ~visited(i)
             visited(i)=true;
             
             Neighbors=RegionQuery(i);
-            % ��� i����Χ�ı߽������С����ֵ����i�㶨Ϊ�����㣨�����߽����ǹ��
+            % 如果 i点周围的边界点数量小于阈值，则将i点暂定为噪声点
+            % 少数边界点躺枪，而且本例程到最后也没将isnoise中被误判的点通过与IDX对比改正过来
+            % 所以会出现有些点既是边界点又是噪声点的情况，此时以IDX为准
             if numel(Neighbors)<MinPts
-                % �����������Ҳ�Ǳ߽����������isnoise(i)=true;IDX(i)=n;
+                % 会出现噪声点也是边界点的情况，即isnoise(i)=true;IDX(i)=n;
                 isnoise(i)=true;
             else
                 C=C+1;
@@ -26,7 +28,7 @@ function [IDX, isnoise]=DBSCANdemo(X,epsilon,MinPts)
     end
     
     function ExpandCluster(i,Neighbors,C)
-        % �����ĵ�i��Ϊ������C�࣬������չC��Ĵ�
+        % 将核心点i设为新增的C类，尝试拓展C类的簇
         IDX(i)=C;  
         k = 1;
         while true
@@ -35,17 +37,17 @@ function [IDX, isnoise]=DBSCANdemo(X,epsilon,MinPts)
             if ~visited(j)
                 visited(j)=true;
                 Neighbors2=RegionQuery(j);
-                % ���j��Ҳ�Ǻ��ĵ㣬����j�ı߽��Ҳ���뵽�������
+                % 如果j点也是核心点，就让j的边界点也加入到这个簇中
                 if numel(Neighbors2)>=MinPts
                     Neighbors=[Neighbors Neighbors2];   %#ok
                 end
             end
-            % �����ں��ĵ�ͱ߽�㶼��ΪC��
+            % 将簇内核心点和边界点都设为C类
             if IDX(j)==0
                 IDX(j)=C;
             end
             k = k + 1;
-            % ���û���µı߽��������
+            % 如果没有新的边界点加入簇中
             if k > numel(Neighbors)
                 break;
             end
